3812933be6097f913a54978bf604519f575cb3a3
Hashim sharif <97352574+hashim1sharif@users.noreply.github.com>
2025-11-08 13:15:32 +0100

    Delete Labs directory

--- Labs/Terraform/modules/ec2/ec2.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_instance" "this" {
-  ami                     = local.instance_ami
-  instance_type           = var.instance_type
-}
-resource "aws_instance" "imported" {
-  ami                     = local.instance_ami
-  instance_type           = var.instance_type
-  tags = {"Name" = "import-terraform"}
-}
--- Labs/bash/backup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Create Backup directory in bash_script/
-mkdir -p Arena/Backup
-
-# Copy .txt files from Arena to Backup
-cp Arena/*.txt Arena/Backup/
-
